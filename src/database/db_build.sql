fbd5cb3b97d769762e3f5dbaf225f7b26b1d8c8c
charlie la fosse <charlielafosse@outlook.com>
2018-12-05 15:40:46 +0000

    add second table

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -5,11 +5,17 @@ CREATE TABLE restaurants (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     location VARCHAR(300) NOT NULL,
-    img_url VARCHAR(200)
+    img_url VARCHAR(200) DEFAULT "https://cdn2.iconfinder.com/data/icons/map-locations-filled-pixel-perfect/64/pin-map-location-19-512.png"
 );
 
--- Second table: Users 
+
+-- Second table: Users
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  github_username VARCHAR(100) NOT NULL,
+  name VARCHAR(100) NOT NULL
+);
 -- Third table: Posts
--- Forth table: Comments 
+-- Forth table: Comments
 
 COMMIT;
