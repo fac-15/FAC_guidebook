0365d05635a38d7aa560f00b6e9433f2fbf52863
Sandra <grahlsan@gmail.com>
2018-12-05 16:46:08 +0000

    server up and running

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -5,7 +5,8 @@ CREATE TABLE restaurants (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   location VARCHAR(300) NOT NULL,
-  img_url VARCHAR(200) DEFAULT "https://cdn2.iconfinder.com/data/icons/map-locations-filled-pixel-perfect/64/pin-map-location-19-512.png"
+  img_url VARCHAR(200) 
+  -- DEFAULT "https://cdn2.iconfinder.com/data/icons/map-locations-filled-pixel-perfect/64/pin-map-location-19-512.png"
 );
 
 -- Second table: Users
