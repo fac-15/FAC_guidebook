9c1fb85c309f6d29dd144b6a02aadc78eac82970
Whooolia <tpdms2779@gmail.com>
2018-12-05 16:30:36 +0000

    Insert values into restaurant table

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -4,8 +4,9 @@ BEGIN;
 CREATE TABLE restaurants (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
-  location VARCHAR(300) NOT NULL,
-  img_url VARCHAR(200) DEFAULT "https://cdn2.iconfinder.com/data/icons/map-locations-filled-pixel-perfect/64/pin-map-location-19-512.png"
+  location VARCHAR(300) NOT NULL
+  -- img_url VARCHAR(300)
+  -- "https://cdn2.iconfinder.com/data/icons/map-locations-filled-pixel-perfect/64/pin-map-location-19-512.png"
 );
 
 -- Second table: Users
@@ -30,6 +31,15 @@ CREATE TABLE comments (
   post_id INTEGER REFERENCES posts(id),
   text TEXT NOT NULL,
   rating INTEGER
+);
+
+-- Insert restaurants data
+INSERT INTO restaurants (name, location) VALUES
+(
+  'The Faltering Fullback', '19 Perth Rd, Stroud Green, London N4 3HB'
+),
+(
+  'Yard Sale Pizza', '54 Blackstock Rd, London N4 2DW'
 );
 
 COMMIT;
