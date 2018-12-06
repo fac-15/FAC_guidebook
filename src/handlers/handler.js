23e58ca8e132b018b045b7c1dd0c2f4bd656730e
Whooolia <tpdms2779@gmail.com>
2018-12-06 10:41:55 +0000

    Add handler and test - test passed

--- src/handlers/handler.js
+++ src/handlers/handler.js
@@ -20,6 +20,33 @@ const handlerHome = (request, response) => {
   });
 };
 
+// ----------------------PUBLIC ROUTE ------------
+const handlerPublic = (request, response, url) => {
+  console.log(`requesting the public route, url: ${url}`);
+  const extension = url.split('.')[1];
+  const extensionType = {
+    html: 'text/html',
+    css: 'text/css',
+    js: 'application/javascript',
+  }
+
+  const filePath = path.join(__dirname, "..", "..", url);
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      console.log(`Error: ${error}`);
+      response.writeHead(500, { "Content-Type": "text/html" });
+      response.end("<h1>Sorry, we'v had a problem on our end</h1>");
+    } else {
+      response.writeHead(200, { "Content-Type": extensionType[extension] });
+      response.end(file);
+    }
+  });
+};
+
+
+
+
 module.exports = {
-  handlerHome
+  handlerHome,
+  handlerPublic
 };
