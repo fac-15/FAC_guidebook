cb9a519302ed9efe305bd617a7db62f5542c77c0
Whooolia <tpdms2779@gmail.com>
2018-12-07 12:20:11 +0000

    Main CSS

--- public/js/app.js
+++ public/js/app.js
@@ -31,3 +31,8 @@ fetchData(displayData);
 /// call request functions
 
 // displ// dis
+
+// ***********ARROW BUTTON*******
+document.querySelector('.fas').addEventListener('click', function(){
+  
+})
